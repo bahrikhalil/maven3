11fa0cf9a1ba694c09d2e22e70e0c5505548e21b
bahrikhalil <90874715+bahrikhalil@users.noreply.github.com>
2022-09-30 12:56:41 +0200

    Add files via upload

---
+++ springapp/src/main/java/com/example/fok/FokApplication.java
@@ -0,0 +1,18 @@
+package com.example.fok;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FokApplication {
+
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FokApplication.class, args);
+	}
+
+
+
+}
